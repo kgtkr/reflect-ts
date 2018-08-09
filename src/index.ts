89d4a9c200266ed479dea861dffa18429bfef968
kgtkr <kgtkr.jp@gmail.com>
2018-08-09 22:55:05 +0900

    UnionとIntersection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+type Head<T> = T extends [infer P, ...any[]] ? P : never;
+type Tail<L extends any[]> = ((...x: L) => void) extends ((h: any, ...rest: infer T) => void) ? T : never
+type Fn<R extends any[]> = (...args: R) => void
+type Fn1<H, T extends any[]> = (h: H, ...args: T) => void
+type Cons<H, T extends any[]> = Fn1<H, T> extends Fn<infer R> ? R : []
+
 interface SBoolean {
     type: "boolean"
 }
@@ -44,22 +50,33 @@ interface SObjectMap<T> {
     v: T
 }
 
-interface SUnion<A, B> {
+interface SUnion<T extends any[]> {
     type: "union",
-    a: A,
-    b: B
+    types: T
 }
 
-interface SIntersection<A, B> {
+type Union<T extends any[]> = {
+    0: never,
+    1: V2T<Head<T>>,
+    2: V2T<Head<T>> | Union<Tail<T>>
+}[T extends [] ? 0 : T extends [any] ? 1 : 2];
+
+interface SIntersection<T extends any[]> {
     type: "intersection",
-    a: A,
-    b: B
+    types: T
 }
+
+type Intersection<T extends any[]> = {
+    0: never,
+    1: V2T<Head<T>>,
+    2: V2T<Head<T>> & Union<Tail<T>>
+}[T extends [] ? 0 : T extends [any] ? 1 : 2];
 
 interface SStringLiteral<T extends string> {
     type: "string_literal",
     v: T
 }
+
 
 interface SNumberLiteral<T extends number> {
     type: "number_literal",
@@ -82,8 +99,8 @@ type V2T<T> =
     T extends SSymbol ? symbol :
     T extends SObject<infer P> ? { [K in keyof P]: V2T<P[K]> } :
     T extends SObjectMap<infer P> ? { [key: string]: V2T<P> } :
-    //T extends SUnion<infer A, infer B> ? V2T<A> | V2T<B> :
-    //T extends SIntersection<infer A, infer B> ? V2T<A> & V2T<B> :
+    T extends SUnion<infer P> ? Union<P> :
+    T extends SIntersection<infer P> ? Intersection<P> :
     T extends SStringLiteral<infer P> ? P :
     T extends SNumberLiteral<infer P> ? P :
     T extends SBooleanLiteral<infer P> ? P :
