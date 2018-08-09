53375bbffe4ef85866efe977283b30f9fb47442f
kgtkr <kgtkr.jp@gmail.com>
2018-08-10 02:03:32 +0900

    コメントに色々

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,8 @@
+// https://github.com/Microsoft/TypeScript/pull/24897
+
 export type Head<T> = T extends [infer P, ...any[]] ? P : never;
 export type Tail<L extends any[]> = ((...x: L) => void) extends ((h: any, ...rest: infer T) => void) ? T : never
-type Fn<R extends any[]> = (...args: R) => void
-type Fn1<H, T extends any[]> = (h: H, ...args: T) => void
-export type Cons<H, T extends any[]> = Fn1<H, T> extends Fn<infer R> ? R : []
+export type Cons<H, T extends any[]> = ((h: H, ...args: T) => void) extends ((...args: infer R) => void) ? R : []
 export type Reverse<L extends any[], X extends any[]=[]> = {
     1: X, 0: Reverse<Tail<L>, Cons<Head<L>, X>>
 }[L extends [] ? 1 : 0];
