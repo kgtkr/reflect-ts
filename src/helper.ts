cdcce505565396bd0a8de8559d492180392c8c77
kgtkr <kgtkr.jp@gmail.com>
2018-08-10 11:50:27 +0900

    rename

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,8 @@
 // https://github.com/Microsoft/TypeScript/pull/24897
 
 export type Head<T> = T extends [infer P, ...any[]] ? P : never;
-export type Tail<L extends any[]> = ((...x: L) => void) extends ((h: any, ...rest: infer T) => void) ? T : never;
-export type Cons<H, T extends any[]> = ((h: H, ...args: T) => void) extends ((...args: infer R) => void) ? R : [];
-export type Reverse<L extends any[], X extends any[]=[]> = {
-    0: X, 1: Reverse<Tail<L>, Cons<Head<L>, X>>
-}[L extends [] ? 0 : 1];
+export type Tail<T extends any[]> = ((...x: T) => void) extends ((x: any, ...xs: infer P) => void) ? P : never;
+export type Cons<X, T extends any[]> = ((x: X, ...args: T) => void) extends ((...args: infer R) => void) ? R : [];
+export type Reverse<T extends any[], X extends any[]=[]> = {
+    0: X, 1: Reverse<Tail<T>, Cons<Head<T>, X>>
+}[T extends [] ? 0 : 1];
